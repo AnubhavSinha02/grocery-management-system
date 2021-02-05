2ed4a8fe68f08e08140d37bff2aa85f084b2a5fb
AnubhavSinha02 <sinha.anubhav.as@gmail.com>
2021-02-06 03:16:41 +0530

    Updating user registration component

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RegisterService } from '../service/register.service';
+import { User } from '../user/user';
 
 @Component({
   selector: 'app-registration',
@@ -7,7 +10,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  user=new User();
+  msg='';
+  constructor(private _service:RegisterService,private _router:Router) { }
 
   ngOnInit() {
   }
